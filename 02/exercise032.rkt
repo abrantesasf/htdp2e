3fb439ade4469eea1febb5d45c17a1b9f379abf1
Abrantes Araújo Silva Filho <abrantesasf@gmail.com>
2020-03-29 14:02:02 -0300

    Exercício 32

---
+++ 02/exercise032.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise032) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+;;; Exercício 32
+;;; Interactive Programs
+;;; Abrantes Araújo Silva Filho
+;;; abrantesasf@gmail.com
+
+
+;; 10 eventos que computadores futurísticos terão que lidar
+
+; 1) Gestos e movimentos
+; 2) Voz
+; 3) Pensamento
+; 4) Emoções
+; 5) Biofísica corporal
+; 6) Velocidade, aceleração
+; 7) Condições físicas (temperatura, pressão, etc.)
+; 8) Caligrafia
+; 9) Situações de risco
+; 10) Morte
